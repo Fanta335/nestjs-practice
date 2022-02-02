07f24b233ea33d4424cb520a6f7a689c221298da
Fanta335 <fantayusuke5210@gmail.com>
2022-02-02 20:28:56 +0900

    migrate user to mysql database

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,11 +4,15 @@ const config: ConnectionOptions = {
   type: 'mysql',
   host: 'localhost',
   port: 3306,
-  username: 'admin',
-  password: 'admin',
+  username: 'root',
+  password: undefined,
   database: 'test',
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true,
+  migrations: ['dist/src/db/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
 };
 
 export default config;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'ormconfig';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [TypeOrmModule.forRoot(config), CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
